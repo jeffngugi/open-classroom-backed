da355989f8e33c6e2ae525311bfd42ddd3daacbe
jeffngugi <ngugimwathi254@gmail.com>
2019-11-09 23:13:18 +0300

    set CORS and GEt request

--- app.js
+++ app.js
@@ -3,22 +3,40 @@ const express = require('express');
 const app = express();
 
 app.use((req, res, next) => {
-  console.log('Request received!');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+  );
   next();
 });
 
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
+app.use('/api/stuff', (req, res, next) => {
+  const stuff = [
+    {
+      _id: 'oisodaf',
+      title: 'My first thing',
+      description: 'All of the info about my first thing',
+      imageUrl: '',
+      price: 4900,
+      userId: 'wieojdn1'
+    },
+    {
+      _id: 'oisodaf',
+      title: 'My Second thing',
+      description: 'All of the info about my first thing',
+      imageUrl: '',
+      price: 3500,
+      userId: 'wieojdn1'
+    }
+  ];
 
-app.use((req, res, next) => {
-  res.json({ message: 'Your request was successful!' });
+  res.status(200).json(stuff);
   next();
-});
-
-app.use((req, res, next) => {
-  console.log('Response sent successfully!');
 });
 
 module.exports = app;
